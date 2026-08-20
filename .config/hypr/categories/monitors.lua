bcd8609cc1533fcabb5f92ae2b57afd0d510b7b7
bhnord <b.nordemann@aol.com>
2026-08-19 22:39:28 -0400

    udpate

--- .config/hypr/categories/monitors.lua
+++ .config/hypr/categories/monitors.lua
@@ -5,26 +5,32 @@
 -- Laptop
 
 -- monitor = eDP-1, 1440x900@120, 0x0, 1.0
-
--- Desktop
-
--- primary
-
 hl.monitor({
-	output = "DP-2",
-	mode = "3440x1440@144",
+	output = "eDP-1",
+	mode = "1440x900@120",
 	position = "0x0",
 	scale = 1.0,
 })
 
--- secondary
+-- Desktop
 
-hl.monitor({
-	output = "HDMI-A-1",
-	mode = "1920x1080@60",
-	position = "3440x0",
-	scale = 1.0,
-})
+-- -- primary
+--
+-- hl.monitor({
+-- 	output = "DP-2",
+-- 	mode = "3440x1440@144",
+-- 	position = "0x0",
+-- 	scale = 1.0,
+-- })
+--
+-- -- secondary
+--
+-- hl.monitor({
+-- 	output = "HDMI-A-1",
+-- 	mode = "1920x1080@60",
+-- 	position = "3440x0",
+-- 	scale = 1.0,
+-- })
 
 -- tv
 
